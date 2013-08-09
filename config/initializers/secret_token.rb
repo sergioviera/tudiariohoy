e47c8a83af3a65f53327b53888dba50bce9577dc
sergioviera <sergioviera@gmail.com>
2013-08-09 17:09:35 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TuDiarioHoy::Application.config.secret_token = '8397f32ff2aec28efc9838010d1f61b1ee1640687d9e34828436947af6c8dd023227d8e8699107772024556e97c21e5476160c1fa1b228315a2a816a278c622e'
